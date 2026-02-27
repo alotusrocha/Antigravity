ac0ab462ffb84215b1900c5492441bd1206fd140
alotusrocha <everton.srocha@gmail.com>
2026-02-27 07:44:06 -0300

    chore: remove test files with leaked keys

--- list-models.js
+++
@@ -1,7 +0,0 @@
-const key = 'AIzaSyBesx67Ben3tJZlsoZEv8LF2j0aQiDRwxA';
-fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`)
-  .then(res => res.json())
-  .then(data => {
-    const models = data.models.map(m => m.name);
-    console.log(models);
-  });
--- test.js
+++
@@ -1,8 +0,0 @@
-fetch('https://ytkudhablxwfdupawcyc.supabase.co/functions/v1/chat-agent', {
-  method: 'POST',
-  headers: {
-    'Authorization': 'Bearer sb_publishable_gKqhUI4Tx28AfPmx1A7Pdw_Uv6xnl4F',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ prompt: 'teste' })
-}).then(async r => console.log(r.status, await r.text()));
